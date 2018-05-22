4ab5986b17fae96d66d9cab648ad0781a001f6df
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-22 18:21:47 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,5 +2,5 @@ class Person
     attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_lenght, :pant_width
   
     def initialize(attributes)
-      attributes.each
+      attributes.each {|key, value|
 end
